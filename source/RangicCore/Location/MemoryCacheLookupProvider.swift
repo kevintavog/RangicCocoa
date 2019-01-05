82d164412a33b440fe63692e7cde3e1f1378f496
kevintavog <kevin.tavog@zoho.com>
2019-01-05 13:31:09 -0800

    Fix a crashing bug

--- source/RangicCore/Location/MemoryCacheLookupProvider.swift
+++ source/RangicCore/Location/MemoryCacheLookupProvider.swift
@@ -8,15 +8,21 @@ open class MemoryCacheLookupProvider: LookupProvider
 
     open func lookup(latitude: Double, longitude: Double) -> Placename
     {
+        objc_sync_enter(self)
         let key = Location.toDms(latitude, longitude: longitude)
         if let result = MemoryCacheLookupProvider.cache[key] {
             if result.description.count > 0 {
+                objc_sync_exit(self)
                 return result
             }
         }
+        objc_sync_exit(self)
 
         let result = MemoryCacheLookupProvider.innerLookupProvider.lookup(latitude: latitude, longitude: longitude)
+
+        objc_sync_enter(self)
         MemoryCacheLookupProvider.cache[key] = result
+        objc_sync_exit(self)
         return result
     }
 }
